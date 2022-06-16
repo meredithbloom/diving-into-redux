763f6d34315158207fec28dfc5009cf7875641c9
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-16 17:08:35 -0400

    adjusted counter component, no longer relying on redux template which seems to include a counter. see counter.js for commented out class based component in addition to functional component

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, legacy_createStore } from '@reduxjs/toolkit';
+//import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+
+const counterReducer = (state = { counter: 0}, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter+1
+    }
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter -1
+    }
+  }
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter+action.amount
+    }
+  }
+
+
+  return state
+
+}
+
+
+const store = legacy_createStore(counterReducer)
+
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+
+export default store
