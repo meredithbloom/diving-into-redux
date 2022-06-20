7472eb32621585e33092a1b2a7b13e14723d12b5
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-20 16:09:22 -0400

    changed reducer, slice to redux toolkit syntax

--- src/app/store.js
+++ src/app/store.js
@@ -1,47 +1,43 @@
-import { configureStore, legacy_createStore } from '@reduxjs/toolkit';
+import { configureStore, legacy_createStore, createSlice } from '@reduxjs/toolkit';
 //import counterReducer from '../features/counter/counterSlice';
-
 
 const initialState = {counter: 0, showCounter: true}
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) { 
+      state.counter++
+    },
+    decrement(state) { 
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload
+     },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
     }
   }
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    }
-  }
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    }
-  }
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    }
-  }
-  
-  return state
-
-}
+})
 
 
-const store = legacy_createStore(counterReducer)
 
+//const store = legacy_createStore(counterSlice.reducer)
+
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
 
 // export const store = configureStore({
 //   reducer: {
 //     counter: counterReducer,
 //   },
 // });
+
+export const counterActions = counterSlice.actions
+
+
 
 export default store
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import {useSelector, useDispatch, connect} from 'react-redux'
 import classes from './Counter.module.css';
+import { counterActions } from '../app/store'
 
 const Counter = () => {
 	const dispatch = useDispatch()
@@ -8,20 +9,20 @@ const Counter = () => {
 	const show = useSelector(state => state.showCounter)
 
 	const incrementHandler = () => {
-		dispatch({type: 'increment'})
+		dispatch(counterActions.increment())
 	}
 
 	const decrementHandler = () => {
-		dispatch({type: 'decrement'})
+		dispatch(counterActions.decrement())
 	}
 
 	const increaseHandler = () => {
-		dispatch({type: 'increase', amount: 5})
+		dispatch(counterActions.increase(5))
 	}
 
 
 	const toggleCounterHandler = () => {
-		dispatch({type: 'toggle'})
+		dispatch(counterActions.toggleCounter())
 	};
 
 	return (
